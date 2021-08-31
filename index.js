9a6b3328b42bb1b47506a74ff43c1dd228dfb7bb
gabscar <srgabsc@gmail.com>
2021-08-31 11:28:07 -0300

    feat: challenge 31 done

--- index.js
+++ index.js
@@ -1,38 +1,25 @@
 /*
-A loja de carros será nosso desafio final. Na aula anterior, você fez a parte
-do cadastro dos carros. Agora nós vamos começar a deixar ele com cara de
-projeto mesmo.
-Crie um novo repositório na sua conta do GitHub, com o nome do seu projeto.
-Na hora de criar, o GitHub te dá a opção de criar o repositório com um
-README. Use essa opção.
-Após criar o repositório, clone ele na sua máquina.
-Crie uma nova branch chamada `challenge-30`, e copie tudo o que foi feito no
-desafio da aula anterior para esse novo repositório, nessa branch
-`challenge-30`.
-Adicione um arquivo na raiz desse novo repositório chamado `.gitignore`.
-O conteúdeo desse arquivo deve ser somente as duas linhas abaixo:
-node_modules
-npm-debug.log
-Faça as melhorias que você achar que são necessárias no seu código, removendo
-duplicações, deixando-o o mais legível possível, e então suba essa alteração
-para o repositório do seu projeto.
-Envie um pull request da branch `challenge-30` para a `master` e cole aqui
-nesse arquivo, dentro do `console.log`, o link para o pull request no seu
-projeto.
+Agora vamos criar a funcionalidade de "remover" um carro. Adicione uma nova
+coluna na tabela, com um botão de remover.
+Ao clicar nesse botão, a linha da tabela deve ser removida.
+Faça um pull request no seu repositório, na branch `challenge-31`, e cole
+o link do pull request no `console.log` abaixo.
+Faça um pull request, também com a branch `challenge-31`, mas no repositório
+do curso, para colar o link do pull request do seu repo.
 */
 
 console.log(
-  'Link do seu projeto no seu github de forma privada, adicionando aristotelesFerreira'
+  'Link do seu projeto no seu github de forma privada, adicionando anilton.veigaa@gmail.com'
 );
 (function (DOM) {
   'use strict';
 
-  const app= (function(){      
+  const app= (function(){     
     const $inputImage = new DOM('[data-js="inputImage"]');
     const $inputModel = new DOM('[data-js="inputModel"]');
     const $inputYear = new DOM('[data-js="inputYear"]');
     const $inputPlate = new DOM('[data-js="inputPlate"]');
-    const $inputColor = new DOM('[data-js="inputColor"]');   
+    const $inputColor = new DOM('[data-js="inputColor"]');  
     return{
       start: function() {
         this.getCompany();
@@ -58,7 +45,7 @@ console.log(
         let $tbody = new DOM('[data-js="table-car"').get()[0];
 
         $tbody.appendChild(app.addCar());
-        clearForm();
+        this.clearForm;
 
 
         
@@ -74,7 +61,18 @@ console.log(
         const $tdPlate = document.createElement('td');
         const $tdColor = document.createElement('td');
 
-        $image.src = $inputImage.get()[0].value;
+        const $tdDelete = document.createElement('td');
+        const $delButton = document.createElement('button');
+        
+
+        $delButton.style = 'height: 30px;width:60px; background:red; font-weight: 700;'
+        $delButton.innerHTML='Deletar'
+        
+        
+        
+        $tdDelete.appendChild($delButton);
+        $delButton.addEventListener('click',this.deleteItem)
+        $image.src =$inputImage.get()[0].value;
         $tdImage.appendChild($image);
 
         $tdModel.innerText = $inputModel.get()[0].value.replace(/[<>]/g,'');
@@ -87,11 +85,18 @@ console.log(
         $tr.appendChild($tdYear);
         $tr.appendChild($tdPlate);
         $tr.appendChild($tdColor);
+        $tr.appendChild($tdDelete);
       
         $fragment.appendChild($tr);
        
         
         return $fragment;      
+      },
+
+      deleteItem: function(evt){        
+        let element = evt.target;
+        let td = element.parentNode;
+        return td.parentNode.remove();
       },
 
       setCompany: function(){
